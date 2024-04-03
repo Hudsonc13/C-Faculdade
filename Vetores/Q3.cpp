5fa41670b25852f4d589df7f6790e5ed66269a32
Hudson Clementino <hudson24m@gmail.com>
2024-04-03 09:36:10 -0300

    Q3

---
+++ Vetores/Q3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void leitor(int v[], int tam) {
+
+	for (int i = 0; i < tam; i++) {
+
+		std::cin >> v[i];
+
+	}
+
+}
+
+
+void pares(int v[], int tam) {
+	int contador = 0;
+	for (int i = 0; i < tam; i++) {
+
+		if (v[i] % 2 == 0)
+			contador++;
+
+	}
+
+	std::cout << contador;
+}
+
+
+
+
+
+int main() {
+
+	int v[10];
+	int tam = 10;
+
+	leitor(v, tam);
+	pares(v, tam);
+
+
+}
